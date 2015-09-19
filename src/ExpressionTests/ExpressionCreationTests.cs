6f2634b0ffa9d65af75c899fc30cfc7d46a7e5c2
Michał Depta <mhdepta@gmail.com>
2015-09-19 21:50:17 +0200

    Created basic project structure

---
+++ src/ExpressionTests/ExpressionCreationTests.cs
@@ -0,0 +1,32 @@
+﻿using Expressions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace ExpressionTests
+{
+    [TestFixture]
+    public class ExpressionCreationTests
+    {
+        [Test]
+        [ExpectedException(ExpectedException = typeof(ArgumentNullException))]
+        public void ThrowsIfArgumentsNull()
+        {
+            var expression = new RpnExpression(null, new Stack<Operator>());
+        }
+
+        [Test]
+        [ExpectedException(ExpectedException = typeof(ArgumentNullException))]
+        public void ThrowsIfOperatorsNull()
+        {
+            var expression = new RpnExpression(new Queue<Argument>(new[] { new Argument() }), null);
+        }
+
+        [Test]
+        [ExpectedException(ExpectedException = typeof(ArgumentException))]
+        public void ThrowsIfArgumentsEmpty()
+        {
+            var expression = new RpnExpression(new Queue<Argument>(), new Stack<Operator>());
+        }
+    }
+}
---
+++ src/Expressions/RpnExpression.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Expressions
+{
+    public class RpnExpression
+    {
+        public RpnExpression(Queue<Argument> arguments, Stack<Operator> operators)
+        {
+            if (arguments == null)
+            {
+                throw new ArgumentNullException(nameof(arguments));
+            }
+
+            if (operators == null)
+            {
+                throw new ArgumentNullException(nameof(operators));
+            }
+
+            if (!arguments.Any())
+            {
+                throw new ArgumentException("Arguments list cannot be empty");
+            }
+        }
+
+        public double CalculateResult()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
